c968ed313f6eae4173970457681491d0c1aea864
KranthiKumar-Mada <157268632+KranthiKumar-Mada@users.noreply.github.com>
2024-01-21 11:27:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Automation Tester" , "Youtuber", "Engineer"],
+    strings:["Automation Tester", "Engineer"],
     typespeed: 50,
     backspeed: 100,
     backDelay: 1000,
